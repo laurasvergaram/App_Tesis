9fac212e0dfb5c242a29e9a1c5184f6c14aa1b06
davinzifc <47566127+davinzifc@users.noreply.github.com>
2022-09-14 22:50:49 -0500

    :sparkles: feat(Analyzer): added the function of comparing the words with the story

--- Assets/Scripts/SpeechRecognizer.cs
+++ Assets/Scripts/SpeechRecognizer.cs
@@ -10,15 +10,22 @@ public class SpeechRecognizer : MonoBehaviour, ISpeechRecognizerPlugin
     [SerializeField] private Toggle continuousListeningTgle = null;
     [SerializeField] private TMP_Dropdown languageDropdown = null;
     [SerializeField] private TMP_InputField maxResultsInputField = null;
-    [SerializeField] private TextMeshProUGUI resultsTxt = null;
+    [SerializeField] private TextMeshProUGUI resultsTxt = null; //cuadro grande que muestra las palabras que son iwales
+    [SerializeField] private TextMeshProUGUI resultsTxt2 = null;// cuadro prquegno que muestra lo que se esta hablando en el momento
     [SerializeField] private TextMeshProUGUI errorsTxt = null;
 
+    [SerializeField] private TextMeshProUGUI stringA = null; //Texto del reconocimiento de voz 
+    [SerializeField] private TextMeshProUGUI StringB = null; //Texto predefinido del cuento
+
     private SpeechRecognizerPlugin plugin = null;
+    [SerializeField] private string Cuento = null; //String completo del cuento sin mayusculas ni signos de puntuacion
+    private string[] CuentoArray = null;// Array completo del cuento
+    private int count = 0;// contador global para la posicion de la palabra a comparar del cuento
 
     private void Start()
     {
         plugin = SpeechRecognizerPlugin.GetPlatformPluginVersion(this.gameObject.name);
-
+        this.CuentoArray = this.Cuento.Split(' ');// asignacion del array del cuento segun el string del cuento
         startListeningBtn.onClick.AddListener(StartListening);
         stopListeningBtn.onClick.AddListener(StopListening);
         continuousListeningTgle.onValueChanged.AddListener(SetContinuousListening);
@@ -60,11 +67,30 @@ public class SpeechRecognizer : MonoBehaviour, ISpeechRecognizerPlugin
     {
         char[] delimiterChars = { '~' };
         string[] result = recognizedResult.Split(delimiterChars);
-
-        resultsTxt.text = "";
+        
         for (int i = 0; i < result.Length; i++)
         {
-            resultsTxt.text += result[i] + '\n';
+            this.resultsTxt2.text = result[i]; //Muesra lo que se hablo en el momento!!!
+            string[] resultArray = result[i].Split(' '); //Crea un array de string que permite comparar palabra por palabra
+            for (int c = 0; c < resultArray.Length; c++) // este for compara palabra por palabra del plugin vs el cuento
+            {
+                stringA.text = resultArray[c]; //Texto del reconocimiento de voz 
+                StringB.text = CuentoArray[this.count]; // texto predefinido del cuento
+                if (string.Compare(resultArray[c], CuentoArray[this.count]) == 0) //valida si las palabras son iguales
+                {
+                    resultsTxt.text += resultArray[c] + ' ';//Asignacion en el panel de result
+                    if (count < CuentoArray.Length)
+                    {
+                        this.count++; //contador global para la posicion de la palabra del cuanto a comparar
+                    }
+                }
+            }
+            
+        }
+
+        if (count == CuentoArray.Length)
+        {
+            /* SE COLOCA LA FUNCIONALIDAD AL TERMINAR DE COMPARAR PRRO!!!  */
         }
     }
 
